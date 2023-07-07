b6ba163b738aaf557b6d6f59a9b72d3e41c16989
kale-Dev1 <kamarakale4@gmail.com>
2023-07-07 00:35:17 -0500

    form validation

---
+++ ping-coming-soon-page-master/script.js
@@ -0,0 +1,31 @@
+const form = document.getElementById("email-form");
+let email = document.querySelector(".email");
+const emailError = document.querySelector(".error");
+
+email.addEventListener("input", (event) => {
+  if (email.validity.valid) {
+    emailError.textContent = "";
+    emailError.className = "error";
+  } else {
+    showError();
+  }
+});
+
+form.addEventListener("submit", function (e) {
+  // email.addEventListener('input')
+
+  if (!email.validity.valid) {
+    showError();
+    e.preventDefault();
+  }
+});
+
+function showError() {
+  if (email.validity.valueMissing) {
+    emailError.textContent = "You need to enter an email address.";
+  } else if (email.validity.typeMismatch) {
+    emailError.textContent = "Please provide a valid email address";
+  }
+
+  emailError.className = "error active";
+}
